4792f92fdcb02e939e65731e45d4407a7ef62e9c
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2026-05-01 10:17:45 +0600

    navbar customized

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col ">
         <Navbar />
-        {children}
+        <main className="bg-[#F2F3FC]">{children}</main>
       </body>
     </html>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const links = (
+    <div className="flex flex-col lg:flex-row gap-4 text-[#005BC5] font-medium">
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/courses">Courses</Link>
+      </li>
+      <li>
+        <Link href="/myprofile">My Profile</Link>
+      </li>
+    </div>
+  );
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
+    <div className="container mx-auto">
+      <div className="navbar ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -27,49 +41,13 @@ const Navbar = () => {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <p className="text-[#005BC5] font-bold text-xl">SkillSphere</p>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{links}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
